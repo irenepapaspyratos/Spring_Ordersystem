f5eb3ae30d4d47b2b0931a017473c078066d7663
Irene Papaspyratos <irene.papaspyratos@gmail.com>
2022-07-12 16:02:27 +0200

    Initial commit

---
+++ src/main/java/me/myself/i/spring_ordersystem/SpringOrdersystemApplication.java
@@ -0,0 +1,13 @@
+package me.myself.i.spring_ordersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOrdersystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringOrdersystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/myself/i/spring_ordersystem/SpringOrdersystemApplicationTests.java
@@ -0,0 +1,13 @@
+package me.myself.i.spring_ordersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOrdersystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
